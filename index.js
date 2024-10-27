bf901ae2e07b0ca0818c9cdfdf70b4c9c434910a
Dildora <48212@edu.raseko.fi>
2024-10-27 22:55:38 +0200

    fixed post and delete route handler

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
-require('dotenv').config();
-const mongoose = require('mongoose')
-const Note = require('./models/note')
 const express = require('express');
-const cors = require('cors')
-
 const app = express();
-app.use(express.json()); 
-app.use(cors())
+require('dotenv').config();
+const Note = require('./models/note')
 app.use(express.static('dist'))
 
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('path:', request.path)
+  console.log('Body', request.body)
+  console.log('---')
+  next()
+}
 
+  const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+    if (error.name === 'CastError') {
+      return response.status(400).send({error:'malformed id'})
+  }
+  next(error)
+  }
+  
+  const cors = require('cors')
+  
+  app.use(cors())
+  app.use(express.json()); 
+  app.use(requestLogger)
 
+  const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+  }
+  
+  
 app.get('/', (request, response) => {
   response.send('<h1> Hello World! </h1>');   
 });
@@ -19,54 +39,68 @@ app.get('/api/notes', (request, response) => {
   Note.find({}).then(notes=> {
     response.json(notes);
   })
-    .catch(err => {
+    .catch(error => {
       response.status(500).json({ error: 'Failed to retrieve notes' });   
   })
 });
 
-app.get('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id).then(note=>{
-    response.json(note);   
-  })
+app.post('/api/notes', (request, response) => {
+  const body = request.body;
+  
+  console.log('Received POST request body:', body); // Дополнительный вывод
+  
+  if (!body.content) {
+    return response.status(400).json({
+      error: 'content is missing'
+    });
+  }
+  
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  });
+  
+  note.save().then(savedNote =>{
+    response.json(savedNote);
+  });
 });
 
-app.delete('/api/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id).then(note=>{
+    if (note) {
+      response.json(note);
+    }else {
+      response.status(404).end()
+    }
+  })
+  .catch(error => next(error))
+});
+
+app.delete('/api/notes/:id', (request, response, next) => {
   Note.findByIdAndDelete(request.params.id)
     .then(result => {
-      if (result) {
-        response.status(204).end(); // Успешное удаление
-      } else {
-        response.status(404).json({ error: 'Note not found' }); // Запись не найдена
-      }
+        response.status(204).end(); // Successfull delete
     })
-    .catch(error => {
-      console.error('Error deleting note:', error.message);
-      response.status(500).json({ error: 'Server error' }); // Ошибка сервера
-    });
+    .catch(error => next(error));
 });
 
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body;
 
+  const note = {
+    content: body.content,
+    important: body.important
+  }
+
+  Note.findByIdAndUpdate(request.params.id, note, {new:true}) 
+    .then(updatedNote => {
+      response.json(updatedNote);
+    })
+  .catch(error => next(error))
+});
   
-  app.post('/api/notes', (request, response) => {
-    const body = request.body;
-    
-    console.log('Received POST request body:', body); // Дополнительный вывод
-    
-    if (!body.content) {
-      return response.status(400).json({
-        error: 'content is missing'
-      });
-    }
-    
-    const note = new Note({
-      content: body.content,
-      important: body.important || false,
-    });
-    
-    note.save().then(savedNote =>{
-      response.json(savedNote);
-    });
-  })
+app.use(unknownEndpoint);
+app.use(errorHandler); 
   
   const PORT = process.env.PORT || 3001;
   app.listen(PORT, () => {
